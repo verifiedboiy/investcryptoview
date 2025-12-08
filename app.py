d73fab22f8ff8c75ca92630838cf03f4a0e2da5c
verifiedboiy <verifiedboiy@gmail.com>
2025-12-07 23:27:19 -0800

    Set PORT default to 8080

--- app.py
+++ app.py
@@ -1199,5 +1199,5 @@ def update_password():
     return render_template("update_password.html")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
